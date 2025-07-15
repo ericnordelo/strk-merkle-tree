00d73ea029004313ecd407cb7fb7c81aba26eea1
Rojhat Toptamus <rojhattoptamus@gmail.com>
2025-07-15 12:23:27 +0300

    Export hash functions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export { SimpleMerkleTree } from './simple';
 export { StandardMerkleTree } from './standard';
+
+export { poseidonLeafHash, poseidonNodeHash, standardLeafHash, standardNodeHash } from './hashes';
+
+export type { LeafHash, NodeHash } from './hashes';
